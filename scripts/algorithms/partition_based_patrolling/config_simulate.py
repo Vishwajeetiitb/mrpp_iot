0dd1b7023db86426772f0b2f7022746c8153e9b9
vishwajeetiitb <vishwajeetiitb.official@gmail.com>
2022-09-22 11:06:26 +0530

    resolved conflicts in config simulate script

--- scripts/algorithms/partition_based_patrolling/config_simulate.py
+++ scripts/algorithms/partition_based_patrolling/config_simulate.py
@@ -25,9 +25,6 @@ for graph_name in graphs:
     graph_net = nx.read_graphml(graph_path)
     for run_id in range(no_of_runs):
         for algo_name in algos:
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
             how_many_iterations = 1
             if 'iot' in algo_name:
                 how_many_iterations = len(iot_device_ranges)
@@ -50,55 +47,6 @@ for graph_name in graphs:
                     if 'iot' in algo_name: rospy.set_param('/iot_device_range',iot_device_ranges[idx])
 
                     for name in rospy.get_param_names()[4:]:
-=======
->>>>>>> 4302adb0c19f5a9d524d0503bdeff883e70e9c0d
-            for no_agents in no_of_bots:
-                run_start_time = datetime.now()
-                init_locations = " ".join(list(graph_net.nodes())[0:no_agents])
-                os.system("xterm -e roscore & sleep 3")
-                rospy.set_param('/init_locations',init_locations)
-                rospy.set_param('/use_sim_time',True)
-                rospy.set_param('/gui',False)
-                rospy.set_param('/graph',graph_name)
-                rospy.set_param('/init_bots',no_agents)
-                rospy.set_param('done',False)
-                rospy.set_param('/sim_length',30000)
-                rospy.set_param('/algo_name',algo_name)
-                rospy.set_param('/no_of_deads',0)
-                rospy.set_param('/run_id',run_id)
-                rospy.set_param('/random_string','test')
-                os.system("xterm -e rosrun mrpp_sumo sumo_wrapper.py & sleep 3")
-                if 'iot' in algo_name:
-                    for range in iot_device_ranges:
-                        rospy.set_param('/iot_device_range',range)
-                        
-                        os.system("xterm -e rosrun mrpp_sumo "+ algo_name +".py & sleep 3")
-                        for name in rospy.get_param_names():
-                            print(name,':',rospy.get_param(name))
-                else:
-<<<<<<< HEAD
-                    os.system("xterm -e rosrun mrpp_sumo "+ algo_name +".py & sleep 3")
-                    for name in rospy.get_param_names():
-                        print(name,':',rospy.get_param(name))
-
-                os.system("xterm -e rosrun mrpp_sumo command_center.py")
-                run_end_time = datetime.now()
-                os.system("sleep 10")
-                os.system("killall xterm & sleep 3")
-
-                print('Algorithm took', run_end_time-run_start_time,'\n')
-                print('─' * 100)
-
-
-
-
-end_time  = datetime.now()
-
-
-=======
-                    os.system("xterm -e rosrun mrpp_sumo "+ algo_name +".py & sleep 3")
-                    for name in rospy.get_param_names():
->>>>>>> 70c6b3276a95a591674028438ff07abd8597b666
                         print(name,':',rospy.get_param(name))
 
                     os.system("xterm -e rosrun mrpp_sumo sumo_wrapper.py & sleep 3")
@@ -107,21 +55,13 @@ end_time  = datetime.now()
                     os.system("sleep 10")
                     os.system("killall xterm & sleep 3")
 
-<<<<<<< HEAD
                     run_end_time = datetime.now()
                     print('Algorithm took', run_end_time-run_start_time)
                     print('─' * 100,'\n')
-=======
-                print('Algorithm took', run_end_time-run_start_time,'\n')
-                print('─' * 100)
->>>>>>> 70c6b3276a95a591674028438ff07abd8597b666
 
 
 
 
 end_time  = datetime.now()
-
-
->>>>>>> 4302adb0c19f5a9d524d0503bdeff883e70e9c0d
-print('\n','StartTime:',start_time, '| EndTime:',end_time)
+print('\n','StartTime:',start_time, '|EndTime:',end_time,'|Total Time taken:',end_time-start_time)
 
